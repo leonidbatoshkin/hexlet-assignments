4aba2e935f7f14d4ea4fea1a9a0d3d20b9030b63
@hexlet/cli <support@hexlet.io>
2023-03-06 01:17:22 +0600

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -7,36 +7,6 @@ import java.util.Map;
 import java.util.HashMap;
 
 class App {
-    /*public static void main(String[] args) {
-        toString(getWordCount("word text cat apple word map apple word"));
-
-    }
-
-    public static Map<String, Integer> getWordCount(String sentence) {
-        Map<String, Integer> hashmap = new HashMap<>();
-        List<String> words = new ArrayList<>(Arrays.asList(sentence.split(" ")));
-        var counter = 1;
-        for (String word : words) {
-            if (hashmap.containsKey(word)) {
-                var value = hashmap.get(word);
-                hashmap.put(word, ++value);
-            } else {
-                hashmap.put(word, counter);
-            }
-        }
-        return hashmap;
-    }
-
-    public static String toString(Map<String, Integer> wordsHashmap) {
-        var resultString = "{";
-        if (!wordsHashmap.isEmpty()) {
-            for (Map.Entry<String, Integer> words : wordsHashmap.entrySet()) {
-                resultString += "\n  " + words.getKey() + ": " + words.getValue();
-            }
-        }
-        resultString += "}";
-        return resultString;
-    }*/
     public static Map<String, Integer> getWordCount(String sentence) {
         Map<String, Integer> hashmap = new HashMap<>();
         List<String> words = new ArrayList<>(Arrays.asList(sentence.split(" ")));
