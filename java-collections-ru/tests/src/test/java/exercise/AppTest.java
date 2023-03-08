cd71f16118af772f489261a78b8adc354c9e8a2c
@hexlet/cli <support@hexlet.io>
2023-03-08 15:46:21 +0600

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -16,19 +16,28 @@ class AppTest {
         List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2));
         List<Integer> actual = App.take(list, 2);
         assertThat(actual).isEqualTo(expected);
+    }
 
+    @Test
+    void testTake2() {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> expected1 = new ArrayList<>();
         List<Integer> result1 = App.take(list1, 3);
         assertThat(result1).isEqualTo(expected1);
+    }
 
+    @Test
+    void testTake3() {
         List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));;
+        List<Integer> expected2 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
         List<Integer> result2 = App.take(list2, 7);
         assertThat(result2).isEqualTo(expected2);
+    }
 
+    @Test
+    void testTake4() {
         List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        List<Integer> expected3 = new ArrayList<>();;
+        List<Integer> expected3 = new ArrayList<>();
         List<Integer> result3 = App.take(list3, 0);
         assertThat(result3).isEqualTo(expected3);
     }
