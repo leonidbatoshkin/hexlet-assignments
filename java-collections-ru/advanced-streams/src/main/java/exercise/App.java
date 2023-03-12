b3fe0c31a76797b5d34f030e0a3d8ceffe7d65c8
@hexlet/cli <support@hexlet.io>
2023-03-12 18:23:16 +0600

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -9,7 +9,7 @@ class App {
     public static String getForwardedVariables(String file) {
         return Arrays.stream(file.split("\n"))
                 .filter(line -> line.startsWith("environment="))
-                .map(line -> line.substring("environment=".length()))
+                .map(line -> line.replaceAll("environment=", ""))
                 .map(line -> line.replaceAll("\"", ""))
                 .map(line -> line.split(","))
                 .flatMap(Stream::of)
