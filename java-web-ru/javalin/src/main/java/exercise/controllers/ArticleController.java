7a93f3308373cef8399d3953984b1e9c05e81c32
@hexlet/cli <support@hexlet.io>
2023-05-29 20:34:09 +0600

    submit java-web-ru/javalin

---
+++ java-web-ru/javalin/src/main/java/exercise/controllers/ArticleController.java
@@ -0,0 +1,123 @@
+package exercise.controllers;
+
+import io.javalin.http.Context;
+
+import io.javalin.http.Handler;
+import io.ebean.PagedList;
+
+import java.util.List;
+
+import exercise.domain.query.QArticle;
+import exercise.domain.Article;
+import exercise.domain.Category;
+import exercise.domain.query.QCategory;
+
+
+public final class ArticleController {
+
+    public static Handler listArticles = ctx -> {
+        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1);
+        int rowsPerPage = 10;
+        int offset = (page - 1) * rowsPerPage;
+
+        PagedList<Article> pagedArticles = new QArticle()
+                .setFirstRow(offset)
+                .setMaxRows(rowsPerPage)
+                .orderBy()
+                .id.asc()
+                .findPagedList();
+
+        List<Article> articles = pagedArticles.getList();
+
+        ctx.attribute("articles", articles);
+        ctx.attribute("page", page);
+        ctx.render("articles/index.html");
+        removeFlashMessage(ctx);
+    };
+
+    public static Handler newArticle = ctx -> {
+        List<Category> categories = new QCategory().findList();
+        ctx.attribute("categories", categories);
+        ctx.render("articles/new.html");
+    };
+
+    public static Handler createArticle = ctx -> {
+        String title = ctx.formParam("title");
+        String body = ctx.formParam("body");
+        long categoryId = ctx.formParamAsClass("categoryId", Long.class).getOrDefault(null);
+
+        Category category = new QCategory()
+                .id.equalTo(categoryId)
+                .findOne();
+
+        Article article = new Article(title, body, category);
+        article.save();
+
+        ctx.sessionAttribute("flash", "Статья успешно создана");
+        ctx.redirect("/articles");
+    };
+
+    public static Handler showArticle = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        Article article = new QArticle()
+                .id.equalTo(id)
+                .findOne();
+
+        ctx.attribute("article", article);
+        ctx.render("articles/show.html");
+    };
+
+    public static Handler editArticle = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        Article article = new QArticle()
+                .id.equalTo(id)
+                .findOne();
+
+        List<Category> categories = new QCategory()
+                .findList();
+
+        ctx.attribute("article", article);
+        ctx.attribute("categories", categories);
+        ctx.render("articles/edit.html");
+    };
+
+    public static Handler updateArticle = ctx -> {
+        String title = ctx.formParam("title");
+        String body = ctx.formParam("body");
+        long categoryId = ctx.formParamAsClass("categoryId", Long.class).getOrDefault(null);
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+
+        new QArticle()
+                .id.equalTo(id)
+                .asUpdate() // Преобразует запрос в UPDATE
+                .set("title", title) // Устанавливаем новое значение поля
+                .set("body", body) // Устанавливаем новое значение поля
+                .set("category_id", categoryId) // Устанавливаем новое значение поля
+                .update(); // Выполняем обновление
+        ctx.sessionAttribute("flash", "Статья успешно обновлена");
+        ctx.redirect("/articles");
+    };
+
+    public static Handler deleteArticle = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        Article article = new QArticle()
+                .id.equalTo(id)
+                .findOne();
+        ctx.attribute("article", article);
+        ctx.render("articles/delete.html");
+    };
+
+    public static Handler destroyArticle = ctx -> {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        new QArticle()
+                .id.equalTo(id)
+                .delete();
+        ctx.sessionAttribute("flash", "Статья успешно удалена");
+        ctx.redirect("/articles");
+    };
+
+    private static void removeFlashMessage(Context ctx) {
+        ctx.sessionAttribute("flash", null);
+    }
+}
